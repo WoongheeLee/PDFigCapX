b83ada7b8419c8191cf33e3d931eceb4447bf0dd
Juan Trelles <juantrelles@gmail.com>
2023-06-22 14:40:29 -0500

    refactoring batch processing, README instructions

--- pdfigcapx/README.md
+++ pdfigcapx/README.md
@@ -4,21 +4,97 @@ Python3 implementation of Li et al. [_Figure and caption extraction from biomedi
 PdfFigCapx extracts figures and captions from PDF documents, and returns the
 extracted content and associated metadata.
 
-Following https://mathspp.com/blog/how-to-create-a-python-package-in-2022 to setup the repository structure.
+## 1. Installation
 
-## Dependencies
+### 1.1 Pre-requisites
 
-The project relies on [ChromeDriver](https://chromedriver.chromium.org/downloads) and pdf2html utility from [Xpdf command line tools](https://www.xpdfreader.com/download.html). The library will look by default for ChromDriver at _/usr/bin_ but you can provide a custom path. For pdf2html, make sure the binaries are added to your PATH or in _/usr/bin_.
+The project relies on [ChromeDriver](https://chromedriver.chromium.org/downloads)
+and pdf2html utility from [Xpdf command line tools](https://www.xpdfreader.com/download.html).
+The library will look by default for ChromDriver at _/usr/bin_ but you can provide
+a custom path. For pdf2html, make sure the binaries are added to your PATH or
+in _/usr/bin_.
 
-## Usage
+### 1.2 Install the Poetry project
 
-## Contribute
+You can install the Poetry project and execute the commands using the poetry
+environment.
 
-This project uses [Poetry](https://python-poetry.org/) for dependency management. After cloning the repository, install the dependencies using ` poetry install`. For VSCode, you can also add the environment to the project using and then installing the dependencies:
+```bash
+// clone the repository
+cd pdfigcapx
+poetry install
+```
+
+## 2. Usage
+
+## 2.1 Process every PDF inside a folder
+
+Process every PDF and create a folder per PDF with the images and captions inside `OUTPUT_FOLDER`:
+
+```bash
+cd pdfigcapx
+poetry run python src/batch_processing.py INPUT_FOLDER ARTIFACTS_FOLDER OUTPUT_FOLDER --logs_path LOGS_FOLDER --num_workers 10
+```
+
+Optional parameters:
+
+- --no-individual-folders: add to avoid creating one folder per PDF and instead
+  add all the outputs to the `OUTPUT_FOLDER`
+- --reprocess-errors: add to reprocess any PDF that generated a processing error
+  on a previous run
+- --logs_path: If not specified, uses the `OUTPUT_FOLDER`
+
+## 2.2 Outputs
+
+For every PDF the script generates:
+
+- `PDF_NAME`.json: output metadata
+- One JPG image per image found with the naming convention `PDF_NAME`\_PAGE-NUMBER_IMG-NUMBER.jpg, being the IMG-NUMBER a consecutive number
+  for images inside the page, not the manuscript figure number.
+- `PDF_NAME`.png: Image with thumbnails of every page with the figure and captions
+  location for debugging.
+
+Metadata:
+
+```json
+{
+  "name": PDF_NAME,
+  "xpdf_content_path": location for artifacts,
+  "width": pdf width,
+  "height": pdf height,
+  "pages": [
+    {
+      "number": page number,
+      "figures": [
+        {
+          "bbox": [x_top_left, y_top_left, width, height],
+          "caption": figure caption,
+          "name": figure name if any,
+          "id": file name
+        },
+        ...
+      ]
+    },
+    ...
+  ]
+}
+```
+
+## 3. Contribute
+
+This project uses [Poetry](https://python-poetry.org/) for dependency management.
+After cloning the repository, install the dependencies using ` poetry install`. For
+VSCode, you can also add the environment to the project using and then installing
+the dependencies:
 
 ```bash
 poetry config virtualenvs.in-project true
 peotry install
 ```
 
-Then you can add the interpreter in your IDE. In case you need to delete and reinstall an environment (.venv), check this [post](https://stackoverflow.com/a/64434542).
+Then you can add the interpreter in your IDE. In case you need to delete and
+reinstall an environment (.venv), check this [post](https://stackoverflow.com/a/64434542).
+
+## 4. TODO
+
+- Assumes that every page in the PDF has the same size
--- pdfigcapx/src/batch_processing.py
+++ pdfigcapx/src/batch_processing.py
@@ -1,17 +1,40 @@
+""" Batch processing of PDFs to extract figures and captions.
+Use this script to process all the PDFs that are located in an input folder.
+
+Run:
+poetry run python pdfigcapx/src/batch_preprocessing.py 
+    INPUT_FOLDER            -> folder with PDFs
+    ARTIFACTS_FOLDER        -> folder to place processing artifacts
+    OUTPUTS_FOLDER          -> folder where to place the outputs
+    --no-individual-folders -> add to avoid creating a folder for every
+                               PDF and instead put all the outputs in the OUTPUT FOLDER
+    --num_workers           -> number of processors assigned to process in parallel
+    --batch_size            -> number of documents to process before re-instantiating
+                               a multiprocessing instance. Large numbers can 
+                               flood the memory.
+    --logs_path             -> folder to store logs, if NULL, same as OUTPUT FOLDER
+    --reprocess-errors      -> add to reprocess PDFs marked as errors
+
+The artifacts are PDF pages as images used to find the image and caption 
+coordinates. They can be re-generated, but in case something goes wrong, 
+reprocessing a PDF can re-read these artifacts and avoid re-creating them. We
+recommend to set a temporary location for these elements.
+"""
+
+
 import logging
+from typing import List
+from argparse import Namespace
 import argparse
 import multiprocessing
-from os import listdir
+from os import listdir, makedirs
 from pathlib import Path
 from src.document import Document
-from typing import List
-
-"""
-poetry run python src/batch_processing.py /home/jtt/Documents/datasets/gxd /home/jtt/Documents/outputs/xpdf /home/jtt/Documents/outputs/gdx
-"""
 
 
-def process_pdf(pdf_path: str, xpdf_path: str, data_path: str):
+def process_pdf(
+    pdf_path: str, xpdf_path: str, data_path: str, logs_path: str, create_folder: bool
+):
     """Process each PDF and extract data to data directory.
     Identifies the layout and extracts the figures per page. For bookkeeping, if
     the extraction fails, saves the document name to 'failed_processing.log'. If
@@ -28,99 +51,145 @@ def process_pdf(pdf_path: str, xpdf_path: str, data_path: str):
     - data_path: str
         Full path to folder where to save the extracted images and metadata information
     """
-    error_processing_path = Path(data_path) / "failed_processing.log"
-    error_export_path = Path(data_path) / "failed_export.log"
-    error_draw_path = Path(data_path) / "failed_draw.log"
+    error_processing_path = Path(logs_path) / "pdfigcapx_failed.log"
+    error_export_path = Path(logs_path) / "pdfigcapx_failed_export.log"
+    error_draw_path = Path(logs_path) / "pdfigcapx_failed_draw.log"
+    success_path = Path(logs_path) / "pdfigcapx_success.log"
     document_name = Path(pdf_path).stem
 
     try:
         print(pdf_path)
         # load the document data and identify the pages and regions
-        document = Document(pdf_path, xpdf_path, data_path, include_first_page=False)
+        target_path = data_path
+        if create_folder:
+            target_path = Path(data_path) / document_name
+            makedirs(target_path, exist_ok=True)
+        document = Document(pdf_path, xpdf_path, target_path, include_first_page=False)
         document.extract_figures()
+    # pylint: disable=W0718:broad-exception-caught
     except Exception:
-        logging.error(f"{pdf_path}:", exc_info=True)
-        with open(error_processing_path, "a") as f:
-            f.write(f"{document_name}\n")
+        logging.error(pdf_path, exc_info=True)
+        with open(error_processing_path, "a", encoding="utf-8") as f_in:
+            f_in.write(f"{document_name}\n")
         return
 
     try:
         # save the data to disk
         document.export_metadata(prefix=document_name)
         document.save_images(dpi=300, prefix=document_name)
+    # pylint: disable=W0718:broad-exception-caught
     except Exception:
-        logging.error(f"{pdf_path}:", exc_info=True)
-        with open(error_export_path, "a") as f:
-            f.write(f"{document_name}\n")
+        logging.error(pdf_path, exc_info=True)
+        with open(error_export_path, "a", encoding="utf-8") as f_in:
+            f_in.write(f"{document_name}\n")
+        return
 
     try:
         document.draw(n_cols=10, txtr=True, save=True)
+    # pylint: disable=W0718:broad-exception-caught
     except Exception:
-        logging.warning(f"{pdf_path}:", exc_info=True)
-        with open(error_draw_path, "a") as f:
-            f.write(f"{document_name}\n")
+        logging.warning(pdf_path, exc_info=True)
+        with open(error_draw_path, "a", encoding="utf-8") as f_in:
+            f_in.write(f"{document_name}\n")
+
+    with open(success_path, "a", encoding="utf-8") as f_in:
+        f_in.write(f"{document_name}\n")
 
 
-def filter_not_processed(ids: List[str], logs_path: str) -> List[str]:
+def filters_pdfs(ids: List[str], logs_path: str, reprocess_errors: bool) -> List[str]:
     """Get document names not processed before. Historical data is stored in
     failed_processing.log"""
-    error_processing_path = Path(logs_path) / "failed_processing.log"
+    error_processing_path = Path(logs_path) / "pdfigcapx_failed.log"
 
-    with open(error_processing_path, "r") as f:
-        failed_ids = f.readlines()
-    failed_ids = [x.strip() for x in failed_ids]
+    if not reprocess_errors and error_processing_path.exists():
+        with open(error_processing_path, "r", encoding="utf-8") as f_in:
+            failed_ids = f_in.readlines()
+        failed_ids = [x.strip() for x in failed_ids]
 
-    set_failed_ids = set(failed_ids)
-    set_ids = set(ids)
-    difference = set_ids.difference(set_failed_ids)
-    return list(difference)
+        set_failed_ids = set(failed_ids)
+        set_ids = set(ids)
+        difference = set_ids.difference(set_failed_ids)
+        return list(difference)
+    return ids
 
 
-def batch(iterable, n=256):
+def batch(iterable, n_items=256):
     """Create an iterable to process a long list in batches.
     Needed to process the data in batches and guarantee that we are not filling
     the memory with the data from processes that were already finished.
     """
     # https://stackoverflow.com/questions/8290397/how-to-split-an-iterable-in-constant-size-chunks
-    l = len(iterable)
-    for ndx in range(0, l, n):
-        yield iterable[ndx : min(ndx + n, l)]
+    l_items = len(iterable)
+    for ndx in range(0, l_items, n_items):
+        yield iterable[ndx : min(ndx + n_items, l_items)]
 
 
-if __name__ == "__main__":
+def parse_args():
+    """Read command line arguments"""
     parser = argparse.ArgumentParser(
         prog="pdffigcapx",
         description="batch_processing",
     )
-    parser.add_argument("pdfs_path")
-    parser.add_argument("xpdf_path")
-    parser.add_argument("data_path")
+    parser.add_argument("inputs_path", help="location for inputs")
+    parser.add_argument("xpdf_path", help="location for artifacts")
+    parser.add_argument("outputs_path", help="location for outputs")
+    parser.add_argument(
+        "--no-individual-folders", dest="create_folders", action="store_false"
+    )
+    parser.set_defaults(create_folders=True)
     parser.add_argument("--num_workers", type=int, default=10)
-    args = parser.parse_args()
+    parser.add_argument("--batch_size", type=int, default=256)
+    parser.add_argument("--logs_path", type=str, default=None)
+    parser.add_argument(
+        "--reprocess-errors", dest="reprocess_errors", action="store_true"
+    )
+    parser.set_defaults(reprocess_errors=False)
+    return parser.parse_args()
 
-    input_folder = Path(args.pdfs_path)
-    if not input_folder.exists():
-        raise Exception(f"Input folder {input_folder} does not exist")
 
-    log_path = Path(args.data_path) / "pdfigcapx.log"
+def setup_logging(args: Namespace, level=logging.INFO) -> Path:
+    """Create logging folder and setting logging config"""
+    logs_path = (
+        Path(args.logs_path) if args.logs_path is not None else Path(args.outputs_path)
+    )
+    if not logs_path.exists():
+        makedirs(logs_path, exist_ok=True)
+
+    logfile_path = logs_path / "pdffigcapx.log"
     logging.basicConfig(
-        filename=log_path.resolve(),
+        filename=logfile_path.resolve(),
         filemode="a",
         format="%(asctime)s - %(levelname)s - %(message)s",
-        level=logging.INFO,
+        level=level,
     )
+    return logs_path
+
+
+if __name__ == "__main__":
+    # Process every PDF inside an input folder
+    cmd_args = parse_args()
+    input_folder = Path(cmd_args.inputs_path)
+    if not input_folder.exists():
+        raise FileNotFoundError(f"Input folder {input_folder} does not exist")
+    logging_path = setup_logging(cmd_args, level=logging.INFO)
 
     # Get pdf documents to process
     pdf_names = [el for el in listdir(input_folder) if el.endswith(".pdf")]
-    pdfs_to_process = filter_not_processed(pdf_names, args.data_path)
-    batch_size = 256
+    pdfs_to_process = filters_pdfs(pdf_names, logging_path, cmd_args.reprocess_errors)
+    # create tuples as entries for the multiprocessing starmap
     items = [
-        (input_folder / el, args.xpdf_path, args.data_path) for el in pdfs_to_process
+        (
+            input_folder / el,
+            cmd_args.xpdf_path,
+            cmd_args.outputs_path,
+            logging_path,
+            cmd_args.create_folders,
+        )
+        for el in pdfs_to_process
     ]
 
     # Process data in batches to not collapse the system memory
-    for data_batch in batch(items, n=batch_size):
-        pool = multiprocessing.Pool()
-        with multiprocessing.Pool(args.num_workers) as pool:
+    for data_batch in batch(items, n_items=cmd_args.batch_size):
+        with multiprocessing.Pool(cmd_args.num_workers) as pool:
             pool.starmap(process_pdf, data_batch)
         pool.terminate()
